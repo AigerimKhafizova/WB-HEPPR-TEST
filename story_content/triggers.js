e0c280fc58d4692a60aa9ce016bd8651feb4fecf
AigerimKhafizova <khafizova.aigerim@gmail.com>
2025-08-21 02:24:19 +0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6fSQ1O44jdA":
+        Script1();
+        break;
+      case "65mpHbkfGTP":
+        Script2();
+        break;
+      case "6J3zsSnvb35":
+        Script3();
+        break;
+      case "6pCHUJMlbGK":
+        Script4();
+        break;
+      case "6idUfhY1xx7":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
